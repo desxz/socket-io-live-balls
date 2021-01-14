a59fc5d8d35ef4499de5aa56cdd6528e2ba2fdef
desxz <muratgun@posta.mu.edu.tr>
2021-01-14 11:36:26 +0300

    newUser

---
+++ public/javascripts/controller/indexController.js
@@ -0,0 +1,28 @@
+app.controller('indexController', ['$scope','indexFactory',($scope,indexFactory) => {
+    
+    $scope.init = ()=>{
+        const username = prompt('Enter username');
+
+        if(username){
+            initSocket(username)
+        }else{
+            return false;
+        }
+    }
+    
+    function initSocket(username){
+        const connectionOptions = {
+            reconnectionAttempts:3,
+            reconnectionDelay:600
+        }
+        
+        indexFactory.connectSocket('http://localhost:3000/', connectionOptions)
+        .then((socket)=>{
+            socket.emit('newUser', {username});
+        }).catch((socket)=>{
+            console.log('Error', socket);
+        });
+    }
+
+   
+}])
